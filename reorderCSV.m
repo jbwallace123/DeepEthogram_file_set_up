4203e64c54d41616f656c628356ca91d9580d4b6
jbwallace123 <41006280+jbwallace123@users.noreply.github.com>
2023-03-01 09:07:45 -0500

    Update reorderCSV.m
    Updated to include flexibility for files run with old version of convertLowSpeedAVIs.py

--- reorderCSV.m
+++ reorderCSV.m
@@ -1,4 +1,6 @@
 %% readCSV
+%for new codec code that handles short videos and long for Kim's two
+%different rigs
 
 csv_path ='C:\Users\berri\OneDrive\Desktop\TEST';
 myFiles = dir(fullfile(csv_path,'*.csv'));
@@ -9,17 +11,29 @@ for i=1:length(myFiles)
     fullFileName = fullfile(csv_path, baseFileName);
     fprintf(1, 'Now reading %s\n', fullFileName);
     
-    csv = readtable(fullFileName, 'PreserveVariableNames',true); 
-
+    csv = readtable(fullFileName,'PreserveVariableNames',true); 
+    
+      if ~exist('Var6')
+          Var6 = [];
+          Var6 = zeros(length(csv.Var5),1);
+          Var6(csv.Var2==0 & csv.Var3==0 & csv.Var4==0 & csv.Var5==0)=1;
+      end
+      
+    csv.Var6 = Var6;           
     csv = renamevars(csv, ["Var1", "Var2", "Var3", "Var4", "Var5", "Var6"],...
         ["Frame", "success", "drop", "miss", "missing_pellet", "background"]);
 
     csv_new = [csv(:,1) csv(:,6) csv(:,2:5)];
-
-    save_fname = char(fullFileName);
+    
+    Name = char(fullFileName);
+    Name = Name(1:end-4);
+    save_fname = [Name '_' 'labels' '.csv'];
     writetable(csv_new, save_fname);
 
     disp('File Saved');
-end
+    end
+ 
+   
+        
 
     
